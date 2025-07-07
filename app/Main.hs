bb2404f3feee61d02abbaa522a6f535112e47d3f
cutsea110 <cutsea110@gmail.com>
2025-07-07 10:02:31 +0900

    care for cases of invalid inputs

--- app/Main.hs
+++ app/Main.hs
@@ -11,7 +11,7 @@ safeRead s =
     [(x, "")] -> Just x
     _         -> Nothing
 
-data Request = Yes | No | Get Int deriving (Eq, Show)
+data Request = Yes | No | Get Int | Illegal String deriving (Eq, Show)
 
 parse :: String -> [Request]
 parse = catMaybes . map p . lines
@@ -20,9 +20,11 @@ parse = catMaybes . map p . lines
     p "Y" = Just Yes
     p "n" = Just No
     p "N" = Just No
-    p s = fmap Get $ safeRead s
+    p s = case safeRead s of
+      Just i  -> Just (Get i)
+      Nothing -> Just (Illegal s)
 
-data Response  = Welcome Int | Start | Win | Lose | Turn Int Int | Invalid Int deriving Show
+data Response  = Welcome Int | Start | Win | Lose | Turn Int Int | Invalid Int | Error State deriving Show
 
 pprint :: [Response] -> String
 pprint = unlines . map p
@@ -33,9 +35,13 @@ pprint = unlines . map p
     p Win         = "You win! Continue? (y/n)"
     p Lose        = "You lose! Continue? (y/n)"
     p (Turn x n)  = "I got " ++ show x ++ ". then the rest is " ++ show n ++ ". Your turn."
-    p (Invalid n) = " You can only get up to " ++ show n ++ " stones. Your turn."
+    p (Invalid n) = "You can only get up to " ++ show n ++ " stones. Your turn."
+    p (Error s)   = "Oops! Invalid input. " ++ again
+      where again = case s of
+              Play n _ -> "The rest is " ++ show n ++ " stones. You can get 1-3 stones. Your turn."
+              Continue _ -> "Continue? (y/n)"
 
-data State = Play Int StdGen | Continue StdGen
+data State = Play Int StdGen | Continue StdGen deriving Show
 
 main :: IO ()
 main = do
@@ -49,22 +55,21 @@ player :: StdGen -> Int -> [Request] -> [Response]
 player gen stones inputs = Welcome stones : unfoldr psi (Play stones gen, inputs)
   where
     psi :: ((State, [Request]) -> Maybe (Response, (State, [Request])))
-    psi (_, [])             = Nothing
+    psi (_, [])               = Nothing
+    psi (state, Illegal _:xs) = Just (Error state, (state, xs))
     psi (Continue g, c:cs)
-      | c == Yes = Just (Start, (Play stones g, cs))
-      | otherwise           = Nothing
-    psi (Play n g, x:xs)
-      | y `notElem` [1,2,3] = Just (Invalid 3, (Play n g, xs))
-      | y > n               = Just (Invalid n, (Play n g, xs))
-      | m == 1              = Just (Win, (Continue g, xs))
-      | m == 0              = Just (Lose, (Continue g, xs))
-      | otherwise           = Just (Turn z' (m-z'), (Play (m-z') g', xs))
-      where y = case x of
-              Get i -> i
-              _     -> error "Unexpected input"
-            m  = n - y
+      | c == Yes              = Just (Start, (Play stones g, cs))
+      | c == No               = Nothing
+      | otherwise             = Just (Error (Continue g), (Continue g, cs))
+    psi (Play n g, Get y:xs)
+      | y `notElem` [1,2,3]   = Just (Invalid 3, (Play n g, xs))
+      | y > n                 = Just (Invalid n, (Play n g, xs))
+      | m == 1                = Just (Win, (Continue g, xs))
+      | m == 0                = Just (Lose, (Continue g, xs))
+      | otherwise             = Just (Turn z' (m-z'), (Play (m-z') g', xs))
+      where m  = n - y
             r  = m `mod` 4
             z = (r-1) `mod` 4
-            (z', g')
-              | z /= 0    = (z, g)
-              | otherwise = randomR (1, min 3 m) g
+            (z', g') | z /= 0    = (z, g)
+                     | otherwise = randomR (1, min 3 m) g
+    psi (Play n g, _:xs)      = Just (Error (Play n g), (Play n g, xs))
